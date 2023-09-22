e0def9b6479fe3552e599b606139ac32a3c5bea1
Yohan Velasquez <yohan.0055@gmail.com>
2023-09-22 15:31:47 -0300

    agregado de redes sociales a mobile

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,6 +2,9 @@
 import { useState } from 'react';
 import { FaBars, FaTimes } from 'react-icons/fa'
 import { Link } from 'react-scroll'
+import { FaGithub, FaLinkedin } from 'react-icons/fa'
+import { HiOutlineMail } from 'react-icons/hi'
+import { BsFillPersonLinesFill } from 'react-icons/bs'
 
 const NavBar = () => {
     const [nav, setNav] = useState(false);
@@ -50,9 +53,16 @@ const NavBar = () => {
                     <ul className='flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500'>
                         {links.map(link => (
                             <li key={link.id} className='px-4 cursor-pointer capitalize py-6'>
-                                <Link onClick={()=>setNav(!nav)} to={link.link} smooth duration={500}>{link.link}</Link>
+                                <Link onClick={() => setNav(!nav)} to={link.link} smooth duration={500}>{link.link}</Link>
                             </li>
+
                         ))}
+                        <div className="flex justify-center items-center absolute space-x-14 bottom-20 left-0 w-full cursor-pointer text-white">
+                            <a target='_blank' rel="noreferrer" href="https://www.linkedin.com/in/yohanrvv18/"><FaLinkedin size={30} /></a>
+                            <a target='_blank' rel="noreferrer" href="https://github.com/YohanRVV"><FaGithub size={30} /></a>
+                            <a target='_blank' rel="noreferrer" href="mailto:yohan.0055@gmail.com"><HiOutlineMail size={30} /></a>
+                            <a target='_blank' rel="noreferrer" download={true} href="/resume.pdf"><BsFillPersonLinesFill size={30} /></a>
+                        </div>
                     </ul>
 
                 )}
--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -40,25 +40,27 @@ const SocialLinks = () => {
         }
     ]
     return (
-        <div className='hidden  lg:flex flex-col top-[35%] left-0 fixed'>
-            <ul>
-                {links.map(link => (
-                    <li key={link.id} className={'flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500' + ' ' + link.style}>
-                        <a
-                            href={link.href}
-                            className='flex justify-between items-center w-full text-white font-bold'
-                            download={link.download}
-                            target='_blank'
-                            rel="noreferrer"
-                        >
-                            {link.child}
-                        </a>
-                    </li>
+        <>
 
-                ))}
+            <div className='hidden lg:flex flex-col top-[35%] left-0 fixed'>
+                <ul>
+                    {links.map(link => (
+                        <li key={link.id} className={'flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500' + ' ' + link.style}>
+                            <a
+                                href={link.href}
+                                className='flex justify-between items-center w-full text-white font-bold'
+                                download={link.download}
+                                target='_blank'
+                                rel="noreferrer"
+                            >
+                                {link.child}
+                            </a>
+                        </li>
 
-            </ul>
-        </div>
+                    ))}
+                </ul>
+            </div>
+        </>
     )
 }
 
