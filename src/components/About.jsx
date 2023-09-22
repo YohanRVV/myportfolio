d799f4e8d0de9fb010cd9773ec5e72bdbdf9c14b
Yohan Velasquez <yohan.0055@gmail.com>
2023-09-22 13:39:22 -0300

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,20 @@
+const About = () => {
+    return (
+        <div name="about" className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
+            <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+                <div className='pb-8'>
+                    <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
+                </div>
+                <p className='text-xl mt-20'>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi cum atque veniam earum consequuntur, magni repellat molestiae libero minima nostrum quaerat corporis perspiciatis eveniet eius harum architecto hic laborum consequatur. Cupiditate nostrum et maxime suscipit!
+                </p>
+                <br />
+                <p className='text-xl'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi praesentium consequuntur, numquam id delectus illum exercitationem facere reprehenderit hic blanditiis optio sit, inventore eaque ipsa, alias commodi. Sint debitis, sed, accusamus quae quibusdam illum deleniti tempore eius rem odio culpa, illo laborum blanditiis! Esse laborum sint architecto voluptate debitis alias.
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default About
